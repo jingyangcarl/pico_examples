ee9d238ef4ef908bfee4817c4b201003daa4803f
Jing Yang <jingyang.carl.work@gmail.com>
2022-03-31 23:18:22 -0700

    add reference to see output from pico

--- c/hello_usb/hello_usb.c
+++ c/hello_usb/hello_usb.c
@@ -3,6 +3,7 @@
 
 // reference: https://github.com/raspberrypi/pico-sdk (how to build and make pico project)
 // reference: https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf (3.2.2 indicating how to run code via ssh)
+// reference: https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf (4.1 indicates how to use serial port to get output back)
 int main() {
     setup_default_uart();
     printf("Hellp, world!");
