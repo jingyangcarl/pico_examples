0617be13bd11662760d52531ad4626fc7940d543
Jing Yang <jingyang.carl.work@gmail.com>
2022-04-01 18:41:14 -0700

    printed message is sent to ttyACM0 for visualization

--- c/hello_usb/hello_usb.c
+++ c/hello_usb/hello_usb.c
@@ -5,7 +5,11 @@
 // reference: https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf (3.2.2 indicating how to run code via ssh)
 // reference: https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf (4.1 indicates how to use serial port to get output back)
 int main() {
-    setup_default_uart();
-    printf("Hellp, world!");
+    stdio_init_all();
+    while(true) {
+        // TinyUsb needed to be installed to checkout printed message via com port
+        printf("Hellp, world!\n");
+        sleep_ms(1000);
+    }
     return 0;
 }
